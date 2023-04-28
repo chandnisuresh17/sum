a7efdc54bd8415d588ffbb0c1c6d042a206f7370
chandnisuresh17 <chandnisuresh2000@gmail.com>
2023-04-28 10:19:25 +0530

    Initial commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class AddThreeNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15;
+
+        int sum = num1 + num2 + num3;
+
+        System.out.println(" sum = "+ sum);
+    }
+}
